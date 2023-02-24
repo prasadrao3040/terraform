d174d9fa3a8b5d606ad88749c1015aca2ce87b5f
prasadrao3040 <95232894+prasadrao3040@users.noreply.github.com>
2023-02-24 10:27:37 +0530

    Add files via upload

---
+++ main.tf
@@ -0,0 +1,7 @@
+resource "aws_instance" "this" {
+  ami                     = "ami-0e742cca61fb65051"
+  instance_type           = "t2.micro"
+  subnet_id               = "subnet-0c8fed99111e9576e"
+  
+  
+}
---
+++ provider.tf
@@ -0,0 +1,5 @@
+provider "aws" {
+  region     = "ap-south-1"
+  access_key = "AKIAVPNNOMWGTS3PJPNZ"
+  secret_key = "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"
+}
